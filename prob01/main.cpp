c54213d470f70290a5a3aafc4828718bd70fdb32
shrewdrew21 <shrewdrew21@cs104u-3h.acad.fullerton.edu>
2019-08-31 15:42:26 -0700

    diamond

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,10 +1,16 @@
-// Name: 
+// Name:
 // This program displays a diamond pattern to the screen.
 
 #include <iostream>
 
 int main()
 {
-
+  std::cout<<"   *"<<std::endl;
+  std::cout<<"  ***"<<std::endl;
+  std::cout<<" *****"<<std::endl;
+  std::cout<<"*******"<<std::endl;
+  std::cout<<" *****"<<std::endl;
+  std::cout<<"  ***"<<std::endl;
+  std::cout<<"   *"<<std::endl;
   return 0;
 }
